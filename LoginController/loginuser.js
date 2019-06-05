db05feaf37a01549a0560fb43aa3eca13407f5d1
Elmer <elmerptuazon@yahoo.com>
2019-06-05 13:13:03 +0800

    data from variable

--- LoginController/loginuser.js
+++ LoginController/loginuser.js
@@ -1,8 +1,40 @@
+const MYSQL = require('mysql');
+const SETTINGS = require('.././settings.json');
+const DB = MYSQL.createConnection(SETTINGS);
+
+var getUserCredentials;
+
+//check if db has been disconnected
+DB.connect(function(err) {
+  if(err) throw err;
+  console.log('DB Connected');
+})
+
+function showName(login, pass) {
+//sample query builder
+  DB.query('select * from users where username= ? AND password= ?',[login,pass], (err, results) => {
+    if(err) { console.log(err)
+    }else {
+      getUserCredentials =  results;
+    }
+  });
+
+  return getUserCredentials;
+}
+
 module.exports = {
   userCredentials: function(req, res) {
-    var username = "elmer";
-    var password = "test";
+    var username = "admin";
+    var password = "admin";
+ 
+    var name = showName(username, password);
+    
+    return res.render('.././views/Main/home', {
+      data: name
+    });
+    
+    
 
-    res.send(username + " " + password);
+    // return res.send(getUserCredentials);
   }
 };
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+//ISSUE IS NEED TO REFRESH TWICE TO SEE RESULTS
+
+//controller functions
 var { userCredentials } = require("./LoginController/loginuser");
+
+app.get("/", (req, res) => {
+    res.render('Main/home');
+});
 
 app.get("/home", userCredentials);
 
